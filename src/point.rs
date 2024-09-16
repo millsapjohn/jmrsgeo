3bdb61db108049584b8b0aa0f0c0e42c70810597
millsapjohn <jmillsapengineer@gmail.com>
2024-09-16 10:58:18 -0500

    first batch of functionality

--- src/point.rs
+++ src/point.rs
@@ -27,4 +27,30 @@ pub mod Point {
         let dist = (x_val + y_val).sqrt();
         return dist;
     }
+    pub fn dist3d(point1: &Point, point2: &Point) -> f64 {
+        if point1.zcheck == true && point2.zcheck == true {
+            let x_val = (point1.x_coord - point2.x_coord).powi(2);
+            let y_val = (point1.y_coord - point2.y_coord).powi(2);
+            let z_val = (point1.z_coord - point2.z_coord).powi(2);
+            let dist = (x_val + y_val + z_val).sqrt;
+            return dist;
+        }
+    }
+
+    impl Point {
+        fn zcheck(self: &self) -> bool {
+            match self {
+                Point::Pointz => true,
+                Point::Pointzm => true,
+                other => false,
+            }
+        }
+        fn mcheck(self: &self) -> bool {
+            match self {
+                Point::Pointm => true,
+                Point::Pointzm => true,
+                other => false,
+            }
+        }
+    }
 }
