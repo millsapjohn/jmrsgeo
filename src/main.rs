a9c366ebffddc31c5493e1c24c8b857ffee80eab
millsapjohn <jmillsapengineer@gmail.com>
2024-09-16 15:41:13 -0500

    working distance function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+pub mod point;
+
+use point::*;
+
 fn main() {
-    println!("Hello, world!");
+    let point1 = Pointz::new(0.0, 0.0, 0.0);
+    let point2 = Pointxy::new(1.0, 1.0);
+
+    let dist = dist2d(&point1.cast_to_2d(), &point2);
+    println!("{}", dist);
 }
--- src/point.rs
+++ src/point.rs
@@ -1,56 +1,87 @@
-pub mod Point {
-    pub enum Point {
-        Pointxy {
-            x_coord: f64,
-            y_coord: f64,
-        },
-        Pointz {
-            x_coord: f64,
-            y_coord: f64,
-            z_coord: f64,
-        },
-        Pointm {
-            x_coord: f64,
-            y_coord: f64,
-            m_coord: f64,
-        },
-        Pointzm {
-            x_coord: f64,
-            y_coord: f64,
-            z_coord: f64,
-            m_coord: f64,
-        },
-    }
-    pub fn dist2d(point1: &Point, point2: &Point) -> f64 {
-        let x_val = (point1.x_coord - point2.x_coord).powi(2);
-        let y_val = (point1.y_coord - point2.y_coord).powi(2);
-        let dist = (x_val + y_val).sqrt();
-        return dist;
-    }
-    pub fn dist3d(point1: &Point, point2: &Point) -> f64 {
-        if point1.zcheck == true && point2.zcheck == true {
-            let x_val = (point1.x_coord - point2.x_coord).powi(2);
-            let y_val = (point1.y_coord - point2.y_coord).powi(2);
-            let z_val = (point1.z_coord - point2.z_coord).powi(2);
-            let dist = (x_val + y_val + z_val).sqrt;
-            return dist;
-        }
-    }
+pub trait Point2d {
+    fn cast_to_2d(self) -> Pointxy;
+}
 
-    impl Point {
-        fn zcheck(self: &self) -> bool {
-            match self {
-                Point::Pointz => true,
-                Point::Pointzm => true,
-                other => false,
-            }
-        }
-        fn mcheck(self: &self) -> bool {
-            match self {
-                Point::Pointm => true,
-                Point::Pointzm => true,
-                other => false,
-            }
+pub struct Pointxy {
+    x_coord: f64,
+    y_coord: f64,
+}
+impl Pointxy {
+    pub fn new(x_coord: f64, y_coord: f64) -> Pointxy {
+        Pointxy { x_coord, y_coord }
+    }
+    pub fn dist2d(&self, point2: &Pointxy) -> f64 {
+        ((self.x_coord - point2.x_coord).powi(2) + (self.y_coord - point2.y_coord).powi(2)).sqrt()
+    }
+}
+impl Point2d for Pointxy {
+    fn cast_to_2d(self) -> Pointxy {
+        return self;
+    }
+}
+pub struct Pointz {
+    x_coord: f64,
+    y_coord: f64,
+    z_coord: f64,
+}
+impl Pointz {
+    pub fn new(x_coord: f64, y_coord: f64, z_coord: f64) -> Pointz {
+        Pointz {
+            x_coord,
+            y_coord,
+            z_coord,
         }
     }
+}
+impl Point2d for Pointz {
+    fn cast_to_2d(self) -> Pointxy {
+        let new_point = Pointxy::new(self.x_coord, self.y_coord);
+        return new_point;
+    }
+}
+pub struct Pointm {
+    x_coord: f64,
+    y_coord: f64,
+    m_coord: f64,
+}
+impl Pointm {
+    pub fn new(x_coord: f64, y_coord: f64, m_coord: f64) -> Pointm {
+        Pointm {
+            x_coord,
+            y_coord,
+            m_coord,
+        }
+    }
+}
+impl Point2d for Pointm {
+    fn cast_to_2d(self) -> Pointxy {
+        let new_point = Pointxy::new(self.x_coord, self.y_coord);
+        return new_point;
+    }
+}
+pub struct Pointzm {
+    x_coord: f64,
+    y_coord: f64,
+    z_coord: f64,
+    m_coord: f64,
+}
+impl Pointzm {
+    pub fn new(x_coord: f64, y_coord: f64, z_coord: f64, m_coord: f64) -> Pointzm {
+        Pointzm {
+            x_coord,
+            y_coord,
+            z_coord,
+            m_coord,
+        }
+    }
+}
+impl Point2d for Pointzm {
+    fn cast_to_2d(self) -> Pointxy {
+        let new_point = Pointxy::new(self.x_coord, self.y_coord);
+        return new_point;
+    }
+}
+
+pub fn dist2d(point1: &Pointxy, point2: &Pointxy) -> f64 {
+    ((point1.x_coord - point2.x_coord).powi(2) + (point1.y_coord - point2.y_coord).powi(2)).sqrt()
 }
